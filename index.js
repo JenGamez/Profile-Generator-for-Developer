9ce1ca365e77c7663875b54bd5168249cd2e9319
Jennifer Gamez <jenggamez@gmail.com>
2019-11-16 12:27:26 -0800

    linked the HTML and created prompts

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
-const inquirer = require("inquirer");
 const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
 const util = require("util");
+const generateHTML = require('./generateHTML');
 
+const writeFileAsync = util.promisify(fs.writeFile);
 
+function promptUser() {
+   return inquirer.prompt([
+     {
+       type: "input",
+       name: "username",
+       message: "What is your GitHub username?"
+     },
+     {
+       type: "input",
+       name: "FavColor",
+       message: "What is your favorite color?"
+     },
+   ]);
+ }
+ promptUser()
 
-const questions = [
-  
-];
+  .then(function(data) {
+    const html = generateHTML(data);
+
+    return writeFileAsync("index.html", html);
+  })
+  .then(function() {
+    console.log("Successfully wrote to index.html");
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
 
 function writeToFile(fileName, data) {
- 
 }
 
-function init() {
 
-init();
+// function init() {
+// init();
+
+
+
+
+
+
+
+

c97aeabd53971ec381809e28cc29619b0f71fbf1
Jennifer Gamez <jenggamez@gmail.com>
2019-11-16 10:53:50 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+
+
+const questions = [
+  
+];
+
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {
+
+init();
